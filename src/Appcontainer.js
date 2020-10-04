3de721d4d039dc857b4eac2d59b52fa152237eab
Suraj Vishwakarma <67496096+surajsrv11@users.noreply.github.com>
2020-10-04 21:32:23 +0530

    Update Appcontainer.js

--- src/Appcontainer.js
+++ src/Appcontainer.js
@@ -7,46 +7,51 @@ function Appcontainer(props){
             <center className="header">
                 <h1>COVID 19 INFORMATION</h1>
             </center>
-            <div className="container-total">
+           <div className="container">
+            <div className="col col-small">
                 <center>
-                    <h1 className="info">TOTAL CASES</h1>
-                    <h1 className="data">{props.world.cases}</h1>
+                    <h1 className="name">TOTAL CASES</h1>
+                    <h1>{props.world.cases}</h1>
                 </center>    
             </div>
-            <div className="container-today">
+            <div className="col col-small">
                 <center>
-                    <h1 className="info">TODAY CASES </h1>
-                    <h1 className="data">{props.world.todayCases}</h1>
+                    <h1 className="name">TODAY CASES </h1>
+                    <h1 >{props.world.todayCases}</h1>
                 </center>    
             </div>
-            <div className="container-death">
+            <div className="col col-small">
                 <center>
-                    <h1 className="info">TOTAL DEATHS</h1>
-                    <h1 className="data">{props.world.deaths}</h1>
+                    <h1 className="name">TOTAL DEATHS</h1>
+                    <h1>{props.world.deaths}</h1>
                 </center>    
             </div>
-            <div className="container-recovered">
+            </div>
+            <div className="container">
+            <div className="col col-small">
                 <center>
-                    <h1 className="info">RECOVERED</h1>
-                    <h1 className="data">{props.world.recovered}</h1>
+                    <h1 className="name">RECOVERED</h1>
+                    <h1>{props.world.recovered}</h1>
                 </center>    
             </div>
-            <div className="container-active">
+            <div className="col col-small">
                 <center>
-                    <h1 className="info">ACTIVE CASES</h1>
-                    <h1 className="data">{props.world.active}</h1>
+                    <h1 className="name">ACTIVE CASES</h1>
+                    <h1>{props.world.active}</h1>
                 </center>    
             </div>
-            <div className="container-tests">
+            <div className="col col-small">
                 <center>
-                    <h1 className="info">TOTAL TESTS</h1>
-                    <h1 className="data">{props.world.tests}</h1>
+                    <h1 className="name">TOTAL TESTS</h1>
+                    <h1>{props.world.tests}</h1>
                 </center>    
             </div>
-            <div className="container-countries">
+            </div>
+            <div className="container">
+            <div className="col col-big">
                 <center>
-                    <h1 className="info">AFFECTED COUNTRIES</h1>
-                    <h1 className="data">{props.world.affectedCountries}</h1>
+                    <h1 className="name">AFFECTED COUNTRIES</h1>
+                    <h1>{props.world.affectedCountries}</h1>
                         <div>
                             <img src="https://www.flaticon.com/svg/static/icons/svg/197/197484.svg" height="50" width="50"></img>
                             <img src="https://www.flaticon.com/svg/static/icons/svg/197/197419.svg" height="50" width="50"></img>
@@ -56,6 +61,8 @@ function Appcontainer(props){
                         </div>
                 </center> 
             </div>
+            </div>
+
             <div>   
                 <center className="footer">
                     <h1>Source : disease.sh</h1>
